e99e5427c929d53176dabf60b88cf110e5e0ef42
Rubabshah1 <122277869+Rubabshah1@users.noreply.github.com>
2025-08-15 23:28:25 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,14 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8081,
+    // added tcna address here as proxy
+    proxy: {
+      "/api": {
+        target: "https://tcna.lums.edu.pk",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
   plugins: [
     react(),
@@ -20,3 +28,25 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+// import path from "path";
+// import { componentTagger } from "lovable-tagger";
+
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => ({
+//   server: {
+//     host: "::",
+//     port: 8081,
+//   },
+//   plugins: [
+//     react(),
+//     mode === 'development' &&
+//     componentTagger(),
+//   ].filter(Boolean),
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// }));
