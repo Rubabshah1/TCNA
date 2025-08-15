b582816a9f56df9f669d898840690cca508c80db
Rubabshah1 <122277869+Rubabshah1@users.noreply.github.com>
2025-08-15 23:22:42 +0500

    Update app.py

--- backend/scripts/app.py
+++ backend/scripts/app.py
@@ -15,7 +15,8 @@ from mean import mean_calculation
 # Initialize Flask App
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {
-    "origins": ["http://localhost:8081"],
+    # "origins": ["http://localhost:8081"],
+    "origins": ["https://tcna.lums.edu.pk/"]
     "methods": ["GET", "POST", "OPTIONS"],
     "allow_headers": ["Content-Type", "Authorization"],
     "supports_credentials": True
